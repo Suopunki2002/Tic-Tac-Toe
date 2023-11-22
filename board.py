c9a1242e9ecedf8c91e295ff0acbcb7681ce34f7
Alex Suopanki <alex.suopanki@gmail.com>
2023-11-22 19:34:19 +0200

    Refactor board.py

--- board.py
+++ board.py
@@ -17,63 +17,42 @@ class Board:
             8: (2, 1),
             9: (2, 2)
         }
-        self.coord_to_pos = {
-            (0, 0): 1,
-            (0, 1): 2,
-            (0, 2): 3,            
-            (1, 0): 4,
-            (1, 1): 5,
-            (1, 2): 6,            
-            (2, 0): 7,
-            (2, 1): 8,
-            (2, 2): 9
-        }
+        self.coord_to_pos = {v: k for k, v in self.pos_to_coord.items()}
     
     def is_valid_position(self, position: int) -> bool:
-        is_valid_position = position in [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        if is_valid_position:
-            coords = self.pos_to_coord[position]
-            row = coords[0]; col = coords[1]
-            if self.board[row][col] == ' ':
-                return True
-            else:
-                return False
-        else:
+        if position not in self.pos_to_coord:
             return False
+        row, col = self.pos_to_coord[position]
+        return self.board[row][col] == ' '
         
     def update_position(self, position: int, symbol: str) -> None:
-        coords = self.pos_to_coord[position]
-        row = coords[0]; col = coords[1]
+        row, col = self.pos_to_coord[position]
         self.board[row][col] = symbol
         
     def check_winner(self) -> str:
         for i in range(3):
-            # Check rows
-            if self.board[i][0] == self.board[i][1] == self.board[i][2] != ' ':
-                return self.board[i][0]
-            # Check columns
-            if self.board[0][i] == self.board[1][i] == self.board[2][i] != ' ':
+            # Check rows and columns
+            if self.board[i][0] == self.board[i][1] == self.board[i][2] != ' ' \
+            or self.board[0][i] == self.board[1][i] == self.board[2][i] != ' ':
                 return self.board[0][i]
         # Check diagonals
-        if self.board[0][0] == self.board[1][1] == self.board[2][2] != ' ':
-            return self.board[0][0]
-        if self.board[0][2] == self.board[1][1] == self.board[2][0] != ' ':
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] != ' ' \
+        or self.board[0][2] == self.board[1][1] == self.board[2][0] != ' ':
             return self.board[0][2]
         return None
     
     def is_full(self) -> bool:
-        return all(self.board[i][j] != ' ' for i in range(3) for j in range(3))
+        return all(cell != ' ' for row in self.board for cell in row)
     
     def print_board(self) -> None:
-        for i in range(3):
-            row = self.board[i]
+        for i, row in enumerate(self.board):
             printable = []
-            for j in range(3):
-                x = row[j]
-                if x == ' ':
+            for j, cell in enumerate(row):
+                if cell == ' ':
                     printable += str(self.coord_to_pos[(i, j)])
                 else:
-                    printable += x 
+                    printable += cell
             print(" | ".join(printable))
             if i != 2:
                 print("-" * 9)
+        
