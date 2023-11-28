247d7012a324f32e482ebaeffc5b59b3b4ec569d
Alex Suopanki <alex.suopanki@gmail.com>
2023-11-28 15:16:52 +0200

    Rewritten to fit changes in board.py

--- game.py
+++ game.py
@@ -7,49 +7,43 @@ class Game:
         self.board = Board()
         self.player = 'O'
         self.game_over = False
+    
+    def make_move(self, n_of_square:int) -> None:
+        self.board.update_square(n_of_square, self.player)
         
-    def make_move(self, position: int) -> None:
-        self.board.update_position(position, self.player)
-    
-    def winner(self) -> str:
-        return self.board.check_winner()
-
-    def tie(self) -> bool:
-        return self.board.is_full() and self.winner() is None
-    
     def print_current_board(self) -> None:
         print("\n")
         self.board.print_board()
-        
-    def handle_game_over(self) -> None:
-        if self.winner() is not None:
-            self.game_over = True
-            print(f"\nPlayer {self.player} wins!\n")
-        elif self.tie():
-            self.game_over = True
-            print("\nIt's a tie!\n")
     
+    def handle_game_over(self) -> None:
+        if self.board.check_winner() is not None:
+            self.game_over = True
+            print(f"\nPlayer {self.player} won!\n")
+        elif self.board.check_tie():
+            self.game_over = True
+            print(f"\nIt's a tie!\n")
+        
     def switch_player(self) -> None:
         self.player = 'X' if self.player == 'O' else 'O'
-    
+        
     def player_input(self) -> int:
         while True:
             try:
-                move_pos = int(input("Number of the square: "))
-                if self.board.is_valid_position(move_pos):
-                    return move_pos
+                inp = int(input("Choose a square: "))
+                if self.board.valid_square(inp):
+                    return inp
                 else:
                     print("Invalid move! Square must exist and be empty.")
             except ValueError:
-                print("Invalid input! Please enter a number.")
-        
+                print("Invalid input! Please enter a number 1-9.")
+                
     def new_turn(self) -> None:
         self.print_current_board()
         self.handle_game_over()
         if not self.game_over:
             self.switch_player()
             print(f"\nPlayer {self.player}'s turn. "
-                  "Write the number of the square you want to choose. "
-                  "The square must be empty and the number between 1 and 9.")
-            move_pos = self.player_input()
-            self.make_move(move_pos)
+                "Write the number of the square you want to choose. "
+                "The square must be empty and the number 1-9")
+            chosen_square = self.player_input()
+            self.make_move(chosen_square)
