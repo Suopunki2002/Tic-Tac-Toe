1f0a0e7eb8362dade356676f6fb27cc044c9422c
Alex Suopanki <alex.suopanki@gmail.com>
2023-12-05 17:38:08 +0200

    Make it possible to play against the computer

--- game.py
+++ game.py
@@ -1,49 +1,57 @@
 
 from board import Board
+from ai import AI
 
 
 class Game:
     def __init__(self) -> None:
         self.board = Board()
-        self.player = 'O'
         self.game_over = False
-    
-    def make_move(self, n_of_square:int) -> None:
-        self.board.update_square(n_of_square, self.player)
+        self.current_player = 'O'
+
+    def make_move(self, num_of_square: int) -> None:
+        self.board.update_square(num_of_square, self.current_player)
         
     def print_current_board(self) -> None:
-        print("\n")
+        print("\nCurrent board:\n")
         self.board.print_board()
+        print("\n")
     
     def handle_game_over(self) -> None:
         if self.board.check_winner() is not None:
             self.game_over = True
-            print(f"\nPlayer {self.player} won!\n")
+            print(f"Player {self.current_player} won!\n")
         elif self.board.is_tie():
             self.game_over = True
-            print(f"\nIt's a tie!\n")
+            print("It's a tie!\n")
         
     def switch_player(self) -> None:
-        self.player = 'X' if self.player == 'O' else 'O'
+        self.current_player = 'X' if self.current_player == 'O' else 'O'
         
-    def player_input(self) -> int:
+    def move_input(self) -> int:
         while True:
             try:
-                inp = int(input("Choose a square: "))
-                if self.board.valid_square(inp):
-                    return inp
+                square = int(input(f"Player {self.current_player}'s turn. "
+                                   "Choose a square: "))
+                if self.board.is_valid_square(square):
+                    return square
                 else:
-                    print("Invalid move! Square must exist and be empty.")
+                    print("Invalid move! Square must exist and be empty.\n")
             except ValueError:
-                print("Invalid input! Please enter a number 1-9.")
-                
-    def new_turn(self) -> None:
+                print("Invalid input! Please enter a number 1-9.\n")
+    
+    def player_turn(self) -> None:
         self.print_current_board()
         self.handle_game_over()
+        self.switch_player()
         if not self.game_over:
-            self.switch_player()
-            print(f"\nPlayer {self.player}'s turn. "
-                "Write the number of the square you want to choose. "
-                "The square must be empty and the number 1-9")
-            chosen_square = self.player_input()
+            chosen_square = self.move_input()
+            self.make_move(chosen_square)
+    
+    def computer_turn(self) -> None:
+        self.print_current_board()
+        self.handle_game_over()
+        self.switch_player()
+        if not self.game_over:
+            chosen_square = AI(self.current_player).random_move(self.board)
             self.make_move(chosen_square)
