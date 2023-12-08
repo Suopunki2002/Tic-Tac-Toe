0fee3a68627c17a77db96058ad5c7bf7bd9fd6f2
Alex Suopanki <alex.suopanki@gmail.com>
2023-12-08 17:03:26 +0200

    Get minimax-algorithm working and add alpha-beta pruning

--- ai.py
+++ ai.py
@@ -1,42 +1,55 @@
 
-from copy import deepcopy
 from math import inf
-from random import randrange
 
 from board import Board
 
 
 class AI:
-    def __init__(self, player_symbol: str) -> None:
-        self.player = player_symbol
-        self.opponent = "X" if player_symbol == 'O' else 'O'
+    def __init__(self, ai_symbol: str) -> None:
+        self.ai = ai_symbol
+        self.human = "X" if ai_symbol == 'O' else 'O'
 
-    def minimax(self, board: Board, is_maximizing: bool) -> int:
+    def minimax(self, board: Board, depth: int, alpha: int, beta: int,
+                is_maximizing: bool) -> int:
         possible_winner = board.check_winner()
-        if possible_winner is not None:
-            return -1 if possible_winner == self.player else 1
+        if possible_winner == self.ai:
+            return 10 - depth
+        elif possible_winner == self.human:
+            return depth - 10
         elif board.is_tie():
             return 0
         
         if is_maximizing:
             max_eval = -inf
-            for position in board.empty_squares:
-                child = deepcopy(board).update_square(position, self.player)
-                eval = self.minimax(child, False)
+            for square in board.empty_squares():
+                board.update_square(square, self.ai)
+                eval = self.minimax(board, depth + 1, alpha, beta, False)
+                board.update_square(square, ' ') 
                 max_eval = max(max_eval, eval)
+                alpha = max(alpha, eval)
+                if beta <= alpha:
+                    break
             return max_eval
         else:
             min_eval = inf
-            for position in board.empty_squares:
-                child = deepcopy(board)
-                child.update_square(position, self.opponent)
-                eval = self.minimax(child, True)
+            for square in board.empty_squares():
+                board.update_square(square, self.human)
+                eval = self.minimax(board, depth + 1, alpha, beta, True)
+                board.update_square(square, ' ')
                 min_eval = min(min_eval, eval)
+                beta = min(beta, eval)
+                if beta <= alpha:
+                    break
             return min_eval
     
-    def random_move(self, board: Board):
-        rand = 10
-        while rand not in board.empty_squares:
-            rand = randrange(1, 10)
-        return rand
-                
+    def find_best_move(self, board: Board) -> int:
+        best_score = -inf
+        best_move = -1
+        for move in board.empty_squares():
+            board.update_square(move, self.ai)
+            score = self.minimax(board, 0, -inf, inf, False)
+            board.update_square(move, ' ')
+            if score > best_score:
+                best_score = score
+                best_move = move
+        return best_move
