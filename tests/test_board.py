5426d05b37740de1d6eb10e4df0ad60976f791ea
Alex Suopanki <alex.suopanki@gmail.com>
2023-12-09 16:43:01 +0200

    Write tests for board.py

--- tests/test_board.py
+++ tests/test_board.py
@@ -11,13 +11,13 @@ class TestBoardMethods(unittest.TestCase):
         test_board_1.board = ['X', 'O', 'X',
                               'X', 'O', ' ',
                               'O', ' ', ' ']
-        test_list_1 = test_board_1.empty_squares()
-        self.assertEqual(test_list_1, [6, 8, 9])
         test_board_2 = Board()
         test_board_2.board = ['O', ' ', 'X',
                               'X', 'X', 'O',
                               'O', ' ', ' ']
+        test_list_1 = test_board_1.empty_squares()
         test_list_2 = test_board_2.empty_squares()
+        self.assertEqual(test_list_1, [6, 8, 9])
         self.assertEqual(test_list_2, [2, 8, 9])
         
     def test_is_valid_square(self):
@@ -36,11 +36,9 @@ class TestBoardMethods(unittest.TestCase):
         
     def test_check_winner(self):
         test_board = Board()
-        test_board.board = [
-                              'X', ' ', ' ',
-                              ' ', 'X', 'O',
-                              'O', 'X', 'O'
-                              ]
+        test_board.board = ['X', ' ', ' ',
+                            ' ', 'X', 'O',
+                            'O', 'X', 'O']
         self.assertIsNone(test_board.check_winner())
         test_board.update_square(2, 'X')
         self.assertEqual(test_board.check_winner(), 'X')
