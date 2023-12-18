4b2b56fbda96687f486a602db778bf2d550b504e
Alex Suopanki <alex.suopanki@gmail.com>
2023-12-18 14:12:52 +0200

    Remove unoptimized version of the minimax-algorithm

--- src/ai.py
+++ src/ai.py
@@ -1,7 +1,7 @@
 
 from math import inf
 
-from src.board import Board
+from board import Board
 
 
 class AI:
@@ -9,35 +9,9 @@ class AI:
     def __init__(self, ai_symbol: str) -> None:
         self.ai = ai_symbol
         self.human = "X" if ai_symbol == 'O' else 'O'
-        
-    def minimax(self, board: Board, depth: int, is_maximizing: bool) -> int:
-        possible_winner = board.check_winner()
-        if possible_winner == self.ai:
-            return 10 - depth
-        elif possible_winner == self.human:
-            return depth - 10
-        elif board.is_tie():
-            return 0
-        
-        if is_maximizing:
-            max_eval = -inf
-            for square in board.empty_squares():
-                board.update_square(square, self.ai)
-                eval = self.minimax(board, depth + 1, False)
-                max_eval = max(max_eval, eval)
-                board.update_square(square, ' ') 
-            return max_eval
-        else:
-            min_eval = inf
-            for square in board.empty_squares():
-                board.update_square(square, self.human)
-                eval = self.minimax(board, depth + 1, True)
-                min_eval = min(min_eval, eval)
-                board.update_square(square, ' ')
-            return min_eval
 
-    def minimax_pruning(self, board: Board, depth: int, alpha: int, beta: int,
-                        is_maximizing: bool) -> int:
+    def minimax(self, board: Board, depth: int, alpha: int, beta: int,
+                is_maximizing: bool) -> int:
         possible_winner = board.check_winner()
         if possible_winner == self.ai:
             return 10 - depth
@@ -50,7 +24,7 @@ class AI:
             max_eval = -inf
             for square in board.empty_squares():
                 board.update_square(square, self.ai)
-                eval = self.minimax_pruning(
+                eval = self.minimax(
                     board, depth + 1, alpha, beta, False)
                 board.update_square(square, ' ')
                 max_eval = max(max_eval, eval)
@@ -62,7 +36,7 @@ class AI:
             min_eval = inf
             for square in board.empty_squares():
                 board.update_square(square, self.human)
-                eval = self.minimax_pruning(
+                eval = self.minimax(
                     board, depth + 1, alpha, beta, True)
                 board.update_square(square, ' ')
                 min_eval = min(min_eval, eval)
@@ -76,19 +50,7 @@ class AI:
         best_move = -1
         for move in board.empty_squares():
             board.update_square(move, self.ai)
-            score = self.minimax(board, 0, False)
-            board.update_square(move, ' ')
-            if score > best_score:
-                best_score = score
-                best_move = move
-        return best_move
-    
-    def find_best_move_pruning(self, board: Board) -> int:
-        best_score = -inf
-        best_move = -1
-        for move in board.empty_squares():
-            board.update_square(move, self.ai)
-            score = self.minimax_pruning(board, 0, -inf, inf, False)
+            score = self.minimax(board, 0, -inf, inf, False)
             board.update_square(move, ' ')
             if score > best_score:
                 best_score = score
