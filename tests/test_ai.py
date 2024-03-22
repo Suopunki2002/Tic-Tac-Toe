780d05731b76834d902113f5e8b26580c44e4d47
Alex Suopanki <alex.suopanki@gmail.com>
2024-03-22 14:03:14 +0200

    Refactoring

--- tests/test_ai.py
+++ tests/test_ai.py
@@ -4,38 +4,35 @@ from random import randrange
 import unittest
 
 from src.ai import AI
-
+from src.game import Game
 
 
 class TestAIMethods(unittest.TestCase):
     
     def test_minimax_correctness(self):
         ai = AI('X')
-        test_board = Board()
-        test_board.board = ['X', ' ', ' ',
-                            'X', 'O', ' ',
-                            'O', ' ', ' ']
-        self.assertEqual(ai.find_best_move(test_board), 3)
-        self.assertEqual(ai.find_best_move_pruning(test_board), 3)
+        game = Game()
+        game.board = [
+            'X', ' ', ' ',
+            'X', 'O', ' ',
+            'O', ' ', ' '
+        ]
+        self.assertEqual(ai.find_best_move(game), 3)
         
-    def test_performance_gain_from_pruning(self):
+    def test_performance(self):
+        # Test if the best move can consistantly be found
+        # in under a  tenth of a second
         ai = AI('O')
         for _ in range(10):
-            test_board = Board()
+            game = Game()
             three_random_moves = [randrange(1, 10) for _ in range(3)]
             for i in range(3):
                 symbol = 'O' if i == 1 else 'X'
-                test_board.update_square(three_random_moves[i], symbol)
+                game.board[three_random_moves[i] - 1] = symbol
                 
-            no_pruning_start_t = perf_counter()
-            ai.find_best_move(test_board)
-            no_pruning_end_t = perf_counter()
-            no_pruning_time = no_pruning_end_t - no_pruning_start_t
+            start_time = perf_counter()
+            ai.find_best_move(game)
+            end_time = perf_counter()
+            time = end_time - start_time
             
-            pruning_start_t = perf_counter()
-            ai.find_best_move_pruning(test_board)
-            pruning_end_t = perf_counter()
-            pruning_time = pruning_end_t - pruning_start_t
-            
-            self.assertLessEqual(pruning_time, no_pruning_time)
-            
+            self.assertLess(time, 0.1)
